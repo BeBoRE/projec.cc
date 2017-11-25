ab56b11348ed73feb013feb01b904113d6690a03
dotBeFoRE <33968409+dotBeFoRE@users.noreply.github.com>
2017-11-25 22:29:41 +0100

    Added logout,changed login status,login debug now in /login/debug folder

--- server/login/index.php
+++ server/login/index.php
@@ -7,25 +7,32 @@ require $_SERVER['DOCUMENT_ROOT'].'\db.php';
 $stmt = $db->prepare("SELECT * FROM users WHERE userName=? LIMIT 1");
 $stmt->bind_param("s",$username);
 
-if(!empty($_POST['username']) && !empty($_POST['password'])){
-  $username = $_POST['username'];
-  //Password to SHA256
-  $password = hash("SHA256",$_POST['password']);
+if(empty($_SESSION['userid'])){
+    if(!empty($_POST['username']) && !empty($_POST['password'])){
+    $username = $_POST['username'];
+    //Password to SHA256
+    $password = hash("SHA256",$_POST['password']);
 
-  $stmt->execute();
-  $result = $stmt->get_result()->fetch_assoc();
+    $stmt->execute();
+    $result = $stmt->get_result()->fetch_assoc();
 
-  if($result['password'] == $password){
-    header("Content-Type: application/json");
-    echo 'succesful';
+    if($result['password'] == $password){
+      header("Content-Type: application/json");
+      //echo 'succesful';
+      $_SESSION['userid'] = $result['id'];
+      echo 'succesful';
+    }
+    else{
+      echo 'incorrectcreditials';
+    }
   }
   else{
-    echo json_encode(array("error" => "Username and/or password is incorrect"));
+    header('Content-Type: application/json');
+    echo 'nopassuserset';
   }
 }
 else{
-  header('Content-Type: application/json');
-  echo json_encode(array("error" => "Username and/or password not set"));
+  echo 'alreadyloggedon';
 }
 
 ?>
