127165bc078525f50d32a41b514d6776ab9a7f2f
dotBeFoRE <33968409+dotBeFoRE@users.noreply.github.com>
2017-11-25 21:31:00 +0100

    Able to compare post request (password,username) and added test form

--- server/login/index.php
+++ server/login/index.php
@@ -1,11 +1,31 @@
 <?php
 
 session_start();
+header('Content-Type: application/json');
 require $_SERVER['DOCUMENT_ROOT'].'\db.php';
 
-if(isset($_POST['username']) && isset($_POST['password'])){
-  $username = strip_tags($_POST['username']);
-  $password = strip_tags($_POST['password']);
+$stmt = $db->prepare("SELECT * FROM users WHERE userName=? LIMIT 1");
+$stmt->bind_param("s",$username);
+
+if(!empty($_POST['username']) && !empty($_POST['password'])){
+  $username = $_POST['username'];
+  //Password to SHA256
+  $password = hash("SHA256",$_POST['password']);
+
+  $stmt->execute();
+  $result = $stmt->get_result()->fetch_assoc();
+
+  if($result['password'] == $password){
+    header("Content-Type: application/json");
+    echo 'succesful';
+  }
+  else{
+    echo json_encode(array("error" => "Username and/or password is incorrect"));
+  }
+}
+else{
+  header('Content-Type: application/json');
+  echo json_encode(array("error" => "Username and/or password not set"));
 }
 
 ?>
