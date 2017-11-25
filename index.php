83a3b41812f5167f70affcddd7c3d2a077995fe2
dotBeFoRE <33968409+dotBeFoRE@users.noreply.github.com>
2017-11-25 14:31:38 +0100

    Created a server folder for the server files

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-header('Content-Type: application/json');
-$array = array(array('timePosted' => time(),'title' => 'Lorem Ipsum','thumbnail' => 'http://howtorecordpodcasts.com/wp-content/uploads/2012/10/YouTube-Background-Pop-4.jpg','user' => 'Jason'),array('timePosted' => time() - (7 * 24 * 60 * 60),'title' => 'Who this? new phone!','thumbnail' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Michael_Jackson_Cannescropped.jpg/170px-Michael_Jackson_Cannescropped.jpg','user' => 'Some dumbass rick and morty fan'));
-echo json_encode($array);
-
-?>
---
+++ server/index.php
@@ -0,0 +1,8 @@
+<?php
+
+header('Content-Type: application/json');
+$array = array(array('timePosted' => time(),'title' => 'Lorem Ipsum','thumbnail' => 'http://howtorecordpodcasts.com/wp-content/uploads/2012/10/YouTube-Background-Pop-4.jpg','user' => 'Jason'),array('timePosted' => time() - (3 * 24 * 60 * 60),'title' => 'Who this? new phone','thumbnail' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Michael_Jackson_Cannescropped.jpg/170px-Michael_Jackson_Cannescropped.jpg','user' => 'Some dumbass rick and morty fan'),"error" => "");
+echo json_encode($array);
+
+
+?>
