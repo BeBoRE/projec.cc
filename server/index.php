7b2b0ced48e37536076922f4e3a4cb4553e3314e
dotBeFoRE <33968409+dotBeFoRE@users.noreply.github.com>
2018-01-10 21:56:42 +0100

    Finished example

--- server/index.php
+++ server/index.php
@@ -45,11 +45,16 @@
       <div id="header">
 
       </div>
-      <div id="posts">
-        <div class="post">
-          <h3>Example Title</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <span class="username">Example username</span>
+      <div id="feed">
+        <div class="postwrapper">
+          <div class="post">
+            <h3>Example Title</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <div class="userwrapper">
+              <img src="/resources/profilepicture/example.jpg" alt="">
+              <span>NiggerNiggerNiggerNiggerNiggerNiggerNiggerNigger</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
